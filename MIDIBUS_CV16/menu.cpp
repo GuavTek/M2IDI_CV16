49568c186a9f858b5450046e980c08d0e4062396
GuavTek <36814224+GuavTek@users.noreply.github.com>
2022-08-27 18:45:17 +0200

    Reset menu when an invalid state occurs

--- MIDIBUS_CV16/menu.cpp
+++ MIDIBUS_CV16/menu.cpp
@@ -208,22 +208,8 @@ uint8_t Menu_Service(){
 			}
 			break;
 		default:
-			static uint32_t animateTime = 0;
-			if (animateTime <= RTC->MODE0.COUNT.reg){
-				static uint8_t animationStage;
-				animateTime += 800;
-			
-				if (animationStage >= 31) {
-					animationStage = 0;
-				} else {
-					animationStage++;
-				}
-			
-				uint32_t temp = 7 << animationStage;
-				for (uint8_t i = 0; i < 5; i++)	{
-					LM_WriteRow(i, temp >> (12+i));
-				}
-			}
+			// Invalid state, reset menu
+			Menu_Init();
 			break;
 	}
 	
