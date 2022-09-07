282fcd2225323736ba239dd79eff967561e8eb6c
GuavTek <36814224+GuavTek@users.noreply.github.com>
2022-09-07 18:30:21 +0200

    All the kids!

--- MIDIBUS_CV16/menu.cpp
+++ MIDIBUS_CV16/menu.cpp
@@ -92,19 +92,19 @@ extern struct menuNode load_back_n;
 extern struct menuNode edit_group;
 
 const void Enter_Kid()			{ currentNode = currentNode->kid; }
-const void Enter_Env0()			{ chanSel = 0; currentNode = currentNode->kid; }
-const void Enter_Env1()			{ chanSel = 1; currentNode = currentNode->kid; }
-const void Enter_Env2()			{ chanSel = 2; currentNode = currentNode->kid; }
-const void Enter_Env3()			{ chanSel = 3; currentNode = currentNode->kid; }
-const void Exit_Env()			{ chanSel = 0; currentNode = currentNode->kid; }
+const void Enter_Env0()			{ chanSel = 0; Enter_Kid(); }
+const void Enter_Env1()			{ chanSel = 1; Enter_Kid(); }
+const void Enter_Env2()			{ chanSel = 2; Enter_Kid(); }
+const void Enter_Env3()			{ chanSel = 3; Enter_Kid(); }
+const void Exit_Env()			{ chanSel = 0; Enter_Kid(); }
 const void Edit_Bend()			{ menuStatus = Edit_int; var_edit = &bendRange; max_edit = 8; }
-const void Select_Pressure()	{ outMatrix[chanSel & 0b11][chanSel >> 2].type = GOType_t::Pressure; currentNode = currentNode->kid; }
-const void Select_CV()			{ outMatrix[chanSel & 0b11][chanSel >> 2].type = GOType_t::DC; currentNode = currentNode->kid; }
-const void Select_Gate()		{ outMatrix[chanSel & 0b11][chanSel >> 2].type = GOType_t::Gate; currentNode = currentNode->kid; }
+const void Select_Pressure()	{ outMatrix[chanSel & 0b11][chanSel >> 2].type = GOType_t::Pressure; Enter_Kid(); }
+const void Select_CV()			{ outMatrix[chanSel & 0b11][chanSel >> 2].type = GOType_t::DC; Enter_Kid(); }
+const void Select_Gate()		{ outMatrix[chanSel & 0b11][chanSel >> 2].type = GOType_t::Gate; Enter_Kid(); }
 const void Select_Envelope()	{ menuStatus = Edit_int; var_edit = &outMatrix[chanSel & 0b11][chanSel >> 2].env_num; max_edit = 4; outMatrix[chanSel >> 2][chanSel & 0b11].type = GOType_t::Envelope; }
-const void Select_Velocity()	{ outMatrix[chanSel & 0b11][chanSel >> 2].type = GOType_t::Velocity; currentNode = currentNode->kid; }
-const void Select_Clk()			{ outMatrix[chanSel & 0b11][chanSel >> 2].type = GOType_t::CLK; currentNode = currentNode->kid; }
-const void Select_LFO()			{ outMatrix[chanSel & 0b11][chanSel >> 2].type = GOType_t::LFO; currentNode = currentNode->kid; }
+const void Select_Velocity()	{ outMatrix[chanSel & 0b11][chanSel >> 2].type = GOType_t::Velocity; Enter_Kid(); }
+const void Select_Clk()			{ outMatrix[chanSel & 0b11][chanSel >> 2].type = GOType_t::CLK; Enter_Kid(); }
+const void Select_LFO()			{ outMatrix[chanSel & 0b11][chanSel >> 2].type = GOType_t::LFO; Enter_Kid(); }
 const void Select_LFO_Shape()	{ menuStatus = SetLFO; var_edit = &outMatrix[chanSel & 0b11][chanSel >> 2].shape; }
 const void Select_LFO_Freq_Max(){ menuStatus = Edit_32bit; var_edit = &outMatrix[chanSel & 0b11][chanSel >> 2].freq_max; var_monitor = &outMatrix[chanSel & 0b11][chanSel >> 2].freq_current; }
 const void Select_LFO_Freq_Min(){ menuStatus = Edit_32bit; var_edit = &outMatrix[chanSel & 0b11][chanSel >> 2].freq_min; var_monitor = &outMatrix[chanSel & 0b11][chanSel >> 2].freq_current; }
@@ -113,8 +113,8 @@ const void Set_Max_Range()		{ menuStatus = Edit_16bit; var_edit = &outMatrix[cha
 const void Set_Min_Range()		{ menuStatus = Edit_16bit; var_edit = &outMatrix[chanSel & 0b11][chanSel >> 2].min_range; var_monitor = &outMatrix[chanSel & 0b11][chanSel >> 2].currentOut; }
 const void Set_Conf_Slot()		{ menuStatus = Edit_int; var_edit = &confSlot; max_edit = MAX_SLOTS; }
 const void Set_Save_PC()		{ menuStatus = Wait_MIDI; var_edit = &confPC; midiTypeMask = 0b100; }
-const void Save_Config()		{ needSave = true; currentNode = currentNode->kid; }
-const void Load_Config()		{ needLoad = true; currentNode = currentNode->kid; }
+const void Save_Config()		{ needSave = true; Enter_Kid(); }
+const void Load_Config()		{ needLoad = true; Enter_Kid(); }
 const void Set_Group()			{ menuStatus = Edit_int; var_edit = &midi_group; max_edit = 16; }
 const void Set_Env_Atk_Max()	{ menuStatus = Edit_32bit; var_edit = &envelopes[chanSel].att_max; var_monitor = &envelopes[chanSel].att_current; }
 const void Set_Env_Atk_Min()	{ menuStatus = Edit_32bit; var_edit = &envelopes[chanSel].att_min; var_monitor = &envelopes[chanSel].att_current; }
@@ -285,7 +285,7 @@ uint8_t Menu_Service(){
 			if (buttRight) {
 				buttRight = false;
 				menuStatus = menu_status_t::Navigate;
-				currentNode = currentNode->kid;
+				Enter_Kid();
 			}
 			screenChange = true;
 			break;
@@ -316,7 +316,7 @@ uint8_t Menu_Service(){
 				buttRight = false;
 				menuStatus = menu_status_t::Navigate;
 				needScan = true;
-				currentNode = currentNode->kid;
+				Enter_Kid();
 			}
 			screenChange = true;
 			break;
@@ -365,7 +365,7 @@ uint8_t Menu_Service(){
 					bit_edit_stage = 0;
 					menuStatus = menu_status_t::Navigate;
 					needScan = true;
-					currentNode = currentNode->kid;
+					Enter_Kid();
 				} else {
 					if (bit_edit_stage > 3){
 						bit_edit_stage -= 3;
@@ -422,7 +422,7 @@ uint8_t Menu_Service(){
 					bit_edit_stage = 0;
 					menuStatus = menu_status_t::Navigate;
 					needScan = true;
-					currentNode = currentNode->kid;
+					Enter_Kid();
 				} else {
 					if (bit_edit_stage > 3){
 						bit_edit_stage -= 3;
@@ -479,7 +479,7 @@ uint8_t Menu_Service(){
 					bit_edit_stage = 0;
 					menuStatus = menu_status_t::Navigate;
 					needScan = true;
-					currentNode = currentNode->kid;
+					Enter_Kid();
 				} else {
 					if (bit_edit_stage > 3){
 						bit_edit_stage -= 3;
